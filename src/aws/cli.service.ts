3615241c21c0e06300d0c062d59dc841319d59f8
Nelson Odo <nelsonnedum@gmail.com>
2021-10-23 12:25:17 +0100

    configured serverless.yml

--- src/aws/cli.service.ts
+++ src/aws/cli.service.ts
@@ -20,7 +20,8 @@ const execCommand = (cmd: any, callback: any = null, throwError = true) => {
   });
 };
 
-const s_cli = _resolve(__dirname, '../../node_modules/sequelize-cli/lib/sequelize');
+// const s_cli = _resolve(__dirname, '../../node_modules/sequelize-cli/lib/sequelize');
+const s_cli = _resolve(__dirname, '../../node_modules/ts-node');
 // const s_cli = "npm run";
 
 const dropDB = () => execCommand(`${s_cli} db:drop`);
