9bda151f1da3fb59e52f701273bd2addc550d515
Nelson Odo <nelsonnedum@gmail.com>
2021-10-23 22:55:03 +0100

    configured serverless.yml

--- src/aws/cli.service.ts
+++ src/aws/cli.service.ts
@@ -22,7 +22,7 @@ const execCommand = (cmd: any, callback: any = null, throwError = true) => {
 };
 // const s_cli = _resolve(__dirname, '../../node_modules/sequelize-cli/lib/sequelize');
 // const s_cli = _resolve(__dirname, '../../node_modules/ts-node/dist/index.js');
-const s_cli = "npm run";
+const s_cli = "npx run";
 
 const dropDB = () => execCommand(`${s_cli} db:drop`);
 const dbCreate = () => execCommand(`${s_cli} db:create`, null, false);
--- src/aws/lambda-migrate.ts
+++ src/aws/lambda-migrate.ts
@@ -4,10 +4,9 @@ import { resolve  } from "path";
 
 
 const handler = async function (event:APIGatewayProxyEvent, context:Context, callback:any) {
-  // context.callbackWaitsForEmptyEventLoop = false;
 
   const tsNode = resolve(__dirname, '../../node_modules/.bin/ts-node');
-  shell.echo("Hello world!!!")
+  shell.echo("Hello world!!! "+tsNode)
   // shell.exec(`ts-node src/database/umzug/migrate.ts`);
   shell.exec(`${tsNode} src/database/umzug/migrate.ts`);
   // shell.exec(`npx ts-node src/database/umzug/migrate.ts`);
