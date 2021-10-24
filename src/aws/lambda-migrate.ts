a9b26c89ffd5a26591020aee34d90026c06c8a63
Nelson Odo <nelsonnedum@gmail.com>
2021-10-24 07:36:16 +0100

    configured serverless.yml

--- src/aws/lambda-migrate.ts
+++ src/aws/lambda-migrate.ts
@@ -1,6 +1,7 @@
 import {APIGatewayProxyEvent, Context} from 'aws-lambda';
 import shell from 'shelljs';
 import { resolve  } from "path";
+import { exec } from "child_process";
 
 
 const handler = async function (event:APIGatewayProxyEvent, context:Context, callback:any) {
@@ -9,7 +10,8 @@ const handler = async function (event:APIGatewayProxyEvent, context:Context, cal
   shell.echo("Hello world!!! "+tsNode)
   // shell.exec(`ts-node src/database/umzug/migrate.ts`);
   // shell.exec(`${tsNode} src/database/umzug/migrate.ts`);
-  shell.exec(`npx ts-node src/database/umzug/migrate.ts`);
+  exec(`${tsNode} src/database/umzug/migrate.ts`);
+  // shell.exec(`npx ts-node src/database/umzug/migrate.ts`);
 };
 
 export = {
