19f2d54d537fd361e8b29b0cd7949d4fe18ec18b
Nelson Odo <nelsonnedum@gmail.com>
2021-10-24 08:24:56 +0100

    configured serverless.yml

--- src/aws/lambda-migrate.ts
+++ src/aws/lambda-migrate.ts
@@ -7,19 +7,20 @@ import { exec } from "child_process";
 const handler = async function (event:APIGatewayProxyEvent, context:Context, callback:any) {
 
   const s_cli = resolve(__dirname, '../../node_modules/sequelize-cli/lib/sequelize');
-  const tsNode = resolve(__dirname, '../../node_modules/.bin/ts-node');
-  shell.echo("Hello world!!! "+tsNode)
+  // const tsNode = resolve(__dirname, '../../node_modules/.bin/ts-node');
+  // shell.echo("Hello world!!! "+tsNode)
   // shell.exec(`ts-node src/database/umzug/migrate.ts`);
   // shell.exec(`${tsNode} src/database/umzug/migrate.ts`);
   // exec(`${tsNode} src/database/umzug/migrate.ts`);
   // shell.exec(`npx ts-node src/database/umzug/migrate.ts`);
 
   const promise = new Promise(function(resolve, reject) {
-    exec("ls -la");
-
-    exec(`${s_cli} db:drop`);
-
-    exec(`${tsNode} ./src/database/umzug/migrate.ts`, (error, stdout, stderr) => {
+    const options = {
+      maxBuffer: 10000000,
+      env: process.env
+    }
+    exec(`${s_cli} db:drop`, options, (error, stdout, stderr) => {
+      shell.echo(`Running: ${options.env}`)
       if (error) {
         console.error(`error: ${error.message}`);
         shell.echo( `Error running migration: ${error.message} `);
@@ -30,7 +31,22 @@ const handler = async function (event:APIGatewayProxyEvent, context:Context, cal
       }
       console.log(`stdout: ${stdout}`);
       shell.echo('Migration successfully.');
-    })
+    });
+
+    // exec("ls -la");
+
+    // exec(`${tsNode} ./src/database/umzug/migrate.ts`, (error, stdout, stderr) => {
+    //   if (error) {
+    //     console.error(`error: ${error.message}`);
+    //     shell.echo( `Error running migration: ${error.message} `);
+    //   }
+    //   if (stderr) {
+    //     console.log(`stderr: ${stderr}`);
+    //     shell.echo('Error running migration.');
+    //   }
+    //   console.log(`stdout: ${stdout}`);
+    //   shell.echo('Migration successfully.');
+    // })
   })
 
   return promise
