6c2492321f7bd562fbca741b14fba619070b306a
Nelson Odo <nelsonnedum@gmail.com>
2021-10-24 07:44:29 +0100

    configured serverless.yml

--- src/aws/lambda-migrate.ts
+++ src/aws/lambda-migrate.ts
@@ -10,8 +10,26 @@ const handler = async function (event:APIGatewayProxyEvent, context:Context, cal
   shell.echo("Hello world!!! "+tsNode)
   // shell.exec(`ts-node src/database/umzug/migrate.ts`);
   // shell.exec(`${tsNode} src/database/umzug/migrate.ts`);
-  exec(`${tsNode} src/database/umzug/migrate.ts`);
+  // exec(`${tsNode} src/database/umzug/migrate.ts`);
   // shell.exec(`npx ts-node src/database/umzug/migrate.ts`);
+
+  const promise = new Promise(function(resolve, reject) {
+
+    exec(`${tsNode} ./src/database/umzug/migrate.ts`, (error, stdout, stderr) => {
+      if (error) {
+        console.error(`error: ${error.message}`);
+        shell.echo( 'Error running migration.');
+      }
+      if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        shell.echo('Error running migration.');
+      }
+      console.log(`stdout: ${stdout}`);
+      shell.echo('Migration successfully.');
+    })
+  })
+
+  return promise
 };
 
 export = {
