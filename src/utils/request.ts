28dfee95178526ec47428eddea47c7b1f48651e5
Nelson Odo <nelsonnedum@gmail.com>
2021-10-27 11:25:39 +0100

    configured serverless.yml

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,36 +1,39 @@
-import { ErrorResponse } from "../apiresponse/error.response";
+import {ErrorResponse} from "../apiresponse/error.response";
 import axios from "axios";
 import config from "../config/config";
 
 export const verifyReference = async (reference: string) => {
-  console.log(reference, "-=fd-=f-d=-f=d-f=d-f=d-f=df Hello")
-  console.log(`KEY: ${config.PAYSTACK_TEST_SECRET_KEY}`)
-  try {
-    const result = await axios.get(
-      `https://api.paystack.co/transaction/verify/${reference}`,
-      {
-        headers: {
-          Authorization: `Bearer ${config.PAYSTACK_TEST_SECRET_KEY}`,
-          "Content-Type": "application/json",
-        },
-      }
-    );
+    const prod = config.NODE_ENV === "prod" || config.NODE_ENV === "production";
+    const key = prod ? config.PAYSTACK_LIVE_SECRET_KEY : config.PAYSTACK_TEST_SECRET_KEY
 
-    console.log(result.data);
-    console.log(result.headers);
-    console.log(result);
-    if (!result) {
-      throw new ErrorResponse(
-        "Payment verification error, try again or contact support"
-      );
+    console.log(reference, ":::======------ Hello")
+    console.log(`KEY: ${key}`)
+    try {
+        const result = await axios.get(
+            `https://api.paystack.co/transaction/verify/${reference}`,
+            {
+                headers: {
+                    Authorization: `Bearer ${key}`,
+                    "Content-Type": "application/json",
+                },
+            }
+        );
+
+        console.log(result.data);
+        console.log(result.headers);
+        console.log(result);
+        if (!result) {
+            throw new ErrorResponse(
+                "Payment verification error, try again or contact support"
+            );
+        }
+        if (result.data.data?.status === "success") {
+            return true;
+        }
+        return false;
+    } catch (error: any) {
+        console.log(error)
+        return false;
+        // throw new ErrorResponse(error?.response?.data?.message);
     }
-    if (result.data.data?.status === "success") {
-      return true;
-    }
-    return false;
-  } catch (error: any) {
-    console.log(error)
-    return false;
-    // throw new ErrorResponse(error?.response?.data?.message);
-  }
 };
