5b93cd1f56bb981baecd7c8624162b0a90231fc5
Nelson Odo <nelsonnedum@gmail.com>
2021-10-30 09:17:13 +0100

    configured serverless.yml

--- clean.sh
+++ clean.sh
@@ -1,7 +1,7 @@
 rm -rf node_modules/rxjs/src/
 rm -rf node_modules/rxjs/bundles/
-rm -rf node_modules/rxjs/_esm5/
-rm -rf node_modules/rxjs/_esm2015/
+#rm -rf node_modules/rxjs/_esm5/
+#rm -rf node_modules/rxjs/_esm2015/
 rm -rf node_modules/grpc/deps/grpc/third_party/
 rm -rf node_modules/aws-sdk/dist/
 rm -rf node_modules/serverless-plugin-typescript/
