fde70ca91347e358afb4bf4190de448518513094
Nelson Odo <nelsonnedum@gmail.com>
2021-10-30 10:33:34 +0100

    configured serverless.yml

--- clean.sh
+++ clean.sh
@@ -4,4 +4,4 @@ rm -rf node_modules/rxjs/_esm5/
 rm -rf node_modules/rxjs/_esm2015/
 rm -rf node_modules/grpc/deps/grpc/third_party/
 rm -rf node_modules/aws-sdk/dist/
-rm -rf build/node_modules/typescript/ #Remove typescript module
+rm -rf build/node_modules/typescript/ #Remove typescript module on build
